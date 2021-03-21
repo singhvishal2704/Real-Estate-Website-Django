70ddc771533757fcdac1d19df9712ea76d91ebf7
Vishal Singh <singhvishal2704@gmail.com>
2021-03-21 10:08:39 +0530

    Changed Secret Key

--- btre/settings.py
+++ btre/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3nsaxcpj%!yeq)lftdzbh&-(l*sg(z_5!=r^)4@n)5xhr4d=z+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
